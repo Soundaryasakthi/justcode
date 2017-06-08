9c9670206c98f494edeee2b27bd3b6e4446c5a4d
Soundarya S <soundaryasakthi14@gmail.com>
2017-06-08 10:06:52 +0530

    Create rotatevalue.java

---
+++ rotatevalue.java
@@ -0,0 +1,17 @@
+import java.util.io.*;
+public class rotate{
+public static void main(string[] agrs){
+Scanner scan=new Scanner(System.in);
+int n,k;
+n=scan.nextInt();
+int x[]=newInt[];
+int x[]=newInt[n];
+for(int i=0,i<n;i++)
+x[i]=scan.nextInt();
+k=scan.nextInt();
+for(int i=k;i<n;i++)
+System.out.println(x[i]);
+for(int i=0;i<k;i++)
+System.out.println(x[i]);
+}
+}
